53aa66544fd0269a11b2e9222e22fe8310a10048
DeniseYT <denise.ytlin@gmail.com>
2022-04-15 14:58:01 -0700

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Open and close sidebar
+      function openNav() {
+        document.getElementById("mySidebar").style.width = "20%";
+        document.getElementById("mySidebar").style.display = "block";
+      }
+      
+      function closeNav() {
+        document.getElementById("mySidebar").style.display = "none";
+      }
+
+
+      
+// Open Fullscreen
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+// function closeFullscreen() {
+//   if (document.exitFullscreen) {
+//     document.exitFullscreen();
+//   } else if (document.webkitExitFullscreen) { /* Safari */
+//     document.webkitExitFullscreen();
+//   } else if (document.msExitFullscreen) { /* IE11 */
+//     document.msExitFullscreen();
+//   }
+// }
