54fcf33eeb2cae8e58b58427707d0bda68493f3b
DeniseYT <denise.ytlin@gmail.com>
2022-04-16 21:46:35 -0700

    Update homepage.js
    Update formatting and open Fullscreen syntax

--- homepage.js
+++ homepage.js
@@ -1,20 +1,18 @@
 "use strict";
 
 // Open and close sidebar
-      function openNav() {
-        document.getElementById("mySidebar").style.width = "20%";
-        document.getElementById("mySidebar").style.display = "block";
-      }
-      
-      function closeNav() {
-        document.getElementById("mySidebar").style.display = "none";
-      }
+function openNav() {
+  document.getElementById("mySidebar").style.width = "20%";
+  document.getElementById("mySidebar").style.display = "block";
+}
 
+function closeNav() {
+  document.getElementById("mySidebar").style.display = "none";
+}
 
-      
 // Open Fullscreen
-var elem = document.documentElement;
 function openFullscreen() {
+  const elem = document.documentElement;
   if (elem.requestFullscreen) {
     elem.requestFullscreen();
   } else if (elem.webkitRequestFullscreen) { /* Safari */
